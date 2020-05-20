616d0ec9eab05660e80c34283202a4c1af1c1652
rjfmachado <rjfmachado@users.noreply.github.com>
2020-05-20 16:17:12 +0200

    updates for network

--- tf/1.network.tf
+++ tf/1.network.tf
@@ -15,7 +15,7 @@ resource "azurerm_subnet" "appsvc" {
   name                 = "appsvc"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.0.0/24"
+  address_prefixes     = ["10.0.0.0/24"]
 
   delegation {
     name = "delegationappsvc"
@@ -32,7 +32,7 @@ resource "azurerm_subnet" "privateendpoint" {
   name                                           = "privateendpoint"
   resource_group_name                            = azurerm_resource_group.rg.name
   virtual_network_name                           = azurerm_virtual_network.vnet.name
-  address_prefix                                 = "10.0.1.0/24"
+  address_prefixes                               = ["10.0.1.0/24"]
   enforce_private_link_endpoint_network_policies = true
   #
 }
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "vm" {
   name                 = "vm"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 output "vnetname" {
